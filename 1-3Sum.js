1684b576d790bcdb9f1e7ec7b7c68ec82ce6641e
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2022-12-28 23:07:44 +0530

    Day 2 DSA

---
+++ 1-3Sum.js
@@ -0,0 +1,50 @@
+/**
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+ * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Notice that the solution set must not contain duplicate triplets.
+ */
+
+/**
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ */
+
+// Naive Approach
+function threeSum(arr) {
+  arr.sort((a, b) => a - b);
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === 0) {
+          res.push([arr[i], arr[j], arr[k]]);
+        }
+      }
+    }
+  }
+
+  // filtering unique values
+  console.log(new Set(res.map(JSON.stringify)));
+  const unique = Array.from(new Set(res.map(JSON.stringify)), JSON.parse);
+
+  return unique;
+}
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+// console.log(threeSum([0, 0, 0]));
