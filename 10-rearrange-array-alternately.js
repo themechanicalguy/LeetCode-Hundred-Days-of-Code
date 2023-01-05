62a48179f36d7c87f89990ce1adc862d040563ee
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-01-05 22:16:16 +0530

    5-1-22

---
+++ 10-rearrange-array-alternately.js
@@ -0,0 +1,8 @@
+/**
+Given a sorted array of positive integers. Your task is to rearrange the array elements alternatively 
+i.e first element should be max value, second should be min value, third should be second max, 
+fourth should be second min and so on.
+
+Note: Modify the original array itself. Do it without using any extra space. 
+You do not have to return anything.
+ */
