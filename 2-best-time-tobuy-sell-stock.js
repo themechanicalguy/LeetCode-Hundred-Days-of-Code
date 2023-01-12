561472078675cd7a8fef398691c6fb7dec6928b7
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-01-12 23:13:29 +0530

    backlog items

--- 2-best-time-tobuy-sell-stock.js
+++ 2-best-time-tobuy-sell-stock.js
@@ -3,7 +3,8 @@
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Return the maximum profit you can achieve from this transaction. 
+If you cannot achieve any profit, return 0.
 
  
 
