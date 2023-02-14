5b731b5a1d18b3b7c79ffd09f1261633c0874a48
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-02-14 05:37:23 +0530

    hollow full pyramid

--- GFG/0-Basics/Patterns/9-full-pyramid.cpp
+++ GFG/0-Basics/Patterns/9-full-pyramid.cpp
@@ -14,7 +14,7 @@ int main() {
   int n;
   cin>>n;
   for(int row=0;row<n;row++){
-    for(int space = n-1-row;space>=0;space--){
+    for(int space = n-row-1;space>=0;space--){
       cout<<" ";
     }
     for(int j=0;j<row+1;j++){
@@ -22,5 +22,30 @@ int main() {
     }
     cout<<endl;
   }
+  
+}
+
+// other appraoch
+
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin>>n;
+  for(int row=0;row<n;row++){
+    int k=0;
+    for(int col=0;col<(2*n-1);col++){
+      if(col < n-row-1){
+        cout<<" ";
+      }else if(k < 2 * row +1){
+        cout<<"*";
+        k++;
+      }else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
+    
+    }
   
 }
