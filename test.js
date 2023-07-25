519df120dbb3bc611850d84472b00f5da5ee6eca
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-07-25 23:50:16 +0530

    updated

--- test.js
+++ test.js
@@ -1,64 +1,57 @@
-/*
-Write a function to find the longest common prefix string amongst an array of strings.
-If there is no common prefix, return an empty string "".
-
-Example 1:
-
-Input: strs = ["flower","flow","flight"]
-Output: "fl"
-Example 2:
-
-Input: strs = ["dog","racecar","car"]
-Output: ""
-Explanation: There is no common prefix among the input strings.
-
-Approach:
-1- loop through array, and get individual string
-2- //hold 2 strings at once
-3- //compare both the strings and store their matching in a temporary varible
-4- then start checking that variable for the following strings and update accordingly
-5- return the res
-
-//1- Find the smallest string among all, and set it to res -O(n)
-//2- Use charAt and find the 
-
-*/
-const longestCommonPrefix = (strs) => {
-  let res = "";
-  // let smallestLen = "";
-  // strs.forEach((item, i, arr) => {
-  //   let next;
-  //   if (i < arr.length) {
-  //     next = arr[i + 1];
-  //   }
-  //   if (item.length < next?.length) {
-  //     smallestLen = item;
-  //   }
-  // });
-  // console.log(smallestLen);
-  // strs.forEach((item, index, arr) => {
-  //   const t = item.charAt("->", smallestLen);
-  //   console.log("iii", t);
-  // });
-  strs.forEach((item, index, arr) => {
-    //check if it is not the last element
-    let next;
-    if (index + 1 < arr.length) {
-      next = arr[index + 1];
-    }
-    for (let i = 0; i < item.length; i++) {
-      if (next) {
-        if (item[i] === next[i]) {
-          if (!res.charAt(item[i])) res += item[i];
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  //if empty array
+  // if (nums === []) return [-1, -1];
+  const firstOccurence = (nums, target, occurence) => {
+    let s = 0;
+    let e = nums.length - 1;
+    // let res = [-1,-1];
+    let ans = -1;
+    while (s <= e) {
+      let mid = Math.floor(s + (e - s) / 2);
+      console.log(mid, nums[mid]);
+      if (nums[mid] === target) {
+        ans = mid;
+        console.log(ans);
+        if (occurence === "first") {
+          e = mid - 1;
+        } else {
+          s = mid + 1;
         }
+      } else if (nums[mid] < target) {
+        s = mid + 1;
+      } else {
+        e = mid - 1;
       }
     }
-  });
-  // for (let i = 0; i < strs.length;i++) {
-  //   for (let j = 0;j<)
-  // }
+    return ans;
+  };
+  //     const lastOccurence = (nums,target)=>{
+  //     let s = 0;
+  //     let e = nums.length -1;
+  //     // let res = [-1,-1];
+  //     let ans=-1;
 
-  console.log(res);
+  //     while(s <=e){
+  //         let mid = Math.floor(s + (e-s)/2);
+  //         if(nums[mid] === target){
+  //             ans = mid;
+  //             s = mid+1;
+  //         }else if(mid < target){
+  //             s = mid+1;
+  //         }else{
+  //             e = mid-1;
+  //         }
+  //     }
+  //     return ans;
+  // }
+  const first = firstOccurence(nums, target, "first");
+  const last = firstOccurence(nums, target, "last");
+  return [first, last];
 };
 
-console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(searchRange([0, 0, 1, 2, 2], 2));
